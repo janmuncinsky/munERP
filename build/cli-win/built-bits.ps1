e17fd07737b508186b48c9a29d4146bd6fea4313
muncinsky <muncinsky@email.cz>
2018-10-07 20:47:03 +0200

    updating buid script

--- build/cli-win/built-bits.ps1
+++ build/cli-win/built-bits.ps1
@@ -1,14 +1,14 @@
 $ErrorActionPreference='Stop';
 
 # Get-ChildItem output\bin -Recurse | Remove-Item -recurse -force -confirm:$false
-dotnet publish "..\src\MunCode.mERP.sln"  -c Debug
+dotnet publish "src\MunCode.mERP.sln"  -c Debug -v detailed
 
-if ($lastexitcode -ne 0) {
-  throw "Build failed."
-}
+# if ($lastexitcode -ne 0) {
+  # throw "Build failed."
+# }
 
-$msbuild = "c:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
-$sln = "..\src\MunCode.mERP.Client.Win.sln"
-$params = "/p:Configuration=Debug"
+# $msbuild = "c:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
+# $sln = "src\MunCode.mERP.Client.Win.sln"
+# $params = "/p:Configuration=Debug"
 
-& $msbuild $sln $params
+# & $msbuild $sln $params
