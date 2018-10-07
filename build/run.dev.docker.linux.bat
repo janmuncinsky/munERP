8a7c1698d7a7e64e2b348f0b56620162c0bc946d
muncinsky <muncinsky@email.cz>
2018-10-07 21:43:00 +0200

    project renaming

--- build/run.dev.docker.linux.bat
+++ build/run.dev.docker.linux.bat
@@ -1,5 +1,5 @@
-docker-compose -f yml/docker-compose.linux.yml -f yml/docker-compose.override.yml -p munERP up -d --remove-orphans --force-recreate --build || (pause && exit)
+docker-compose -f yml/docker-compose.linux.yml -f yml/docker-compose.override.yml -p munerp up -d --remove-orphans --force-recreate --build || (pause && exit)
 docker ps
-docker-compose -f yml/docker-compose.linux.yml -p munERP --no-ansi logs -f -t
+docker-compose -f yml/docker-compose.linux.yml -p munerp --no-ansi logs -f -t
 pause
 
