741eb7060b0549410df82c6b00d39ce2cac4645b
muncinsky <muncinsky@email.cz>
2018-10-10 10:58:49 +0200

    reduce duplication in yml files

--- build/run.dev.docker.win.bat
+++ build/run.dev.docker.win.bat
@@ -1,5 +1,5 @@
-docker-compose -f yml/docker-compose.win.yml -f yml/docker-compose.override.yml -p munerp up -d --remove-orphans --force-recreate --build || (pause && exit)
+docker-compose -f yml/docker-compose.yml -f yml/docker-compose.win.yml -p munerp up -d --remove-orphans --force-recreate --build || (pause && exit)
 docker ps
-docker-compose -f yml/docker-compose.win.yml -p munerp --no-ansi logs -f -t
+docker-compose -f yml/docker-compose.yml -p munerp --no-ansi logs -f -t
 pause
 
