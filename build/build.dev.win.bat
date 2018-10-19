54aa413d0138875bb18f79f36fda755fa038f385
muncinsky <muncinsky@email.cz>
2018-10-19 18:28:01 +0200

    Update build/build.dev.win.bat

--- build/build.dev.win.bat
+++ build/build.dev.win.bat
@@ -1,3 +1,4 @@
 cd ..\
+SET PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\amd64\
 Powershell.exe -executionpolicy remotesigned -File build\cli-win\built-bits.ps1
 pause
