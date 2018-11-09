a2f06079cedc501a35ff21d7c2f0a29ae8966c6a
muncinsky <muncinsky@email.cz>
2018-11-09 17:46:15 +0100

    fixing target frameworks

--- build/cli-linux/build-bits.sh
+++ build/cli-linux/build-bits.sh
@@ -3,4 +3,30 @@ set -e
 # echo -e "\e[33m\tClearing output"
 # rm -rf --verbose "docker/images/output"
 echo -e "\e[33m\tBuilding the solution"
-dotnet publish src/MunCode.munERP.sln -c Debug
+
+declare -a netcoreProjects=(
+    "src\MunCode.Core.Messaging.Plugin.Castle\MunCode.Core.Messaging.Plugin.Castle.csproj"
+    "src\MunCode.Core.Messaging.Http.Plugin.Castle\MunCode.Core.Messaging.Http.Plugin.Castle.csproj"
+    "src\MunCode.Core.Caching.Memory.Plugin.Castle\MunCode.Core.Caching.Memory.Plugin.Castle.csproj"
+    "src\MunCode.Core.Log4net.Plugin.Castle\MunCode.Core.Log4net.Plugin.Castle.csproj"
+    "src\MunCode.Core.AppHosting.Plugin.Castle\MunCode.Core.AppHosting.Plugin.Castle.csproj"
+    "src\MunCode.Core.Ioc.Castle\MunCode.Core.Ioc.Castle.csproj"
+    "src\MunCode.Core.EFCore.Plugin.Castle\MunCode.Core.EFCore.Plugin.Castle.csproj"
+    "src\MunCode.Core.Messaging.AspNetCore.Plugin.Castle\MunCode.Core.Messaging.AspNetCore.Plugin.Castle.csproj"
+    "src\MunCode.Core.Messaging.EasyNetQ.Plugin.Castle\MunCode.Core.Messaging.EasyNetQ.Plugin.Castle.csproj"
+    "src\MunCode.Core.Messaging.Api.Plugin.Castle\MunCode.Core.Messaging.Api.Plugin.Castle.csproj"
+    "src\MunCode.Core.Messaging.Gateway.Plugin.Castle\MunCode.Core.Messaging.Gateway.Plugin.Castle.csproj"
+    "src\MunCode.munERP.Accounting.Model.EFCore.Plugin.Castle\MunCode.munERP.Accounting.Model.EFCore.Plugin.Castle.csproj"
+    "src\MunCode.munERP.Sales.Model.EFCore.Plugin.Castle\MunCode.munERP.Sales.Model.EFCore.Plugin.Castle.csproj"
+    "src\MunCode.munERP.Accounting.Api\MunCode.munERP.Accounting.Api.csproj"
+    "src\MunCode.munERP.Sales.Api\MunCode.munERP.Sales.Api.csproj"
+    "src\MunCode.munERP.Accounting.Model\MunCode.munERP.Accounting.Model.csproj"
+    "src\MunCode.munERP.Sales.Model\MunCode.munERP.Sales.Model.csproj"
+    "src\MunCode.Core.AspNetCore.Host\MunCode.Core.AspNetCore.Host.csproj"
+    "src\MunCode.Core.NetCore.Host\MunCode.Core.NetCore.Host.csproj"
+)
+	
+for project in "${netcoreProjects[@]}"
+do
+	dotnet publish $project --framework netcoreapp2.1
+done
--- build/cli-win/built-bits.ps1
+++ build/cli-win/built-bits.ps1
@@ -1,14 +1,48 @@
+function Check-ExitCode { 
+	if ($lastexitcode -ne 0) {
+		throw "Build failed."
+	}
+}
+
 $ErrorActionPreference='Stop';
 
 # Get-ChildItem output\bin -Recurse | Remove-Item -recurse -force -confirm:$false
-dotnet publish "src\MunCode.munERP.sln"
 
-if ($lastexitcode -ne 0) {
-   throw "Build failed."
+$allPlatformsProjects = 
+    @{Path="src\MunCode.Core.Messaging.Plugin.Castle\MunCode.Core.Messaging.Plugin.Castle.csproj"},
+    @{Path="src\MunCode.Core.Messaging.Http.Plugin.Castle\MunCode.Core.Messaging.Http.Plugin.Castle.csproj"},
+    @{Path="src\MunCode.Core.Caching.Memory.Plugin.Castle\MunCode.Core.Caching.Memory.Plugin.Castle.csproj"},
+    @{Path="src\MunCode.Core.Log4net.Plugin.Castle\MunCode.Core.Log4net.Plugin.Castle.csproj"},
+    @{Path="src\MunCode.Core.AppHosting.Plugin.Castle\MunCode.Core.AppHosting.Plugin.Castle.csproj"},
+    @{Path="src\MunCode.Core.Ioc.Castle\MunCode.Core.Ioc.Castle.csproj"}
+	
+$netcoreProjects = 
+    @{Path="src\MunCode.Core.EFCore.Plugin.Castle\MunCode.Core.EFCore.Plugin.Castle.csproj"},
+    @{Path="src\MunCode.Core.Messaging.AspNetCore.Plugin.Castle\MunCode.Core.Messaging.AspNetCore.Plugin.Castle.csproj"},
+    @{Path="src\MunCode.Core.Messaging.EasyNetQ.Plugin.Castle\MunCode.Core.Messaging.EasyNetQ.Plugin.Castle.csproj"},
+    @{Path="src\MunCode.Core.Messaging.Api.Plugin.Castle\MunCode.Core.Messaging.Api.Plugin.Castle.csproj"},
+    @{Path="src\MunCode.Core.Messaging.Gateway.Plugin.Castle\MunCode.Core.Messaging.Gateway.Plugin.Castle.csproj"},
+    @{Path="src\MunCode.munERP.Accounting.Model.EFCore.Plugin.Castle\MunCode.munERP.Accounting.Model.EFCore.Plugin.Castle.csproj"},
+    @{Path="src\MunCode.munERP.Sales.Model.EFCore.Plugin.Castle\MunCode.munERP.Sales.Model.EFCore.Plugin.Castle.csproj"},
+    @{Path="src\MunCode.munERP.Accounting.Api\MunCode.munERP.Accounting.Api.csproj"},
+    @{Path="src\MunCode.munERP.Sales.Api\MunCode.munERP.Sales.Api.csproj"},
+    @{Path="src\MunCode.munERP.Accounting.Model\MunCode.munERP.Accounting.Model.csproj"},
+    @{Path="src\MunCode.munERP.Sales.Model\MunCode.munERP.Sales.Model.csproj"},
+    @{Path="src\MunCode.Core.AspNetCore.Host\MunCode.Core.AspNetCore.Host.csproj"},
+    @{Path="src\MunCode.Core.NetCore.Host\MunCode.Core.NetCore.Host.csproj"}
+
+	
+foreach ($item in $allPlatformsProjects) {
+	dotnet publish $item.Path --framework net461
+	Check-ExitCode
+	dotnet publish $item.Path --framework netcoreapp2.1
+	Check-ExitCode
+}
+
+foreach ($item in $netcoreProjects) {
+	dotnet publish $item.Path --framework netcoreapp2.1
+	Check-ExitCode
 }
 
 msbuild "src\MunCode.munERP.Client.Win.sln" /p:Configuration=Debug
-
-if ($lastexitcode -ne 0) {
-   throw "Build failed."
-}
+Check-ExitCode
